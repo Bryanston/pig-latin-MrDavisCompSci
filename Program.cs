e8b8b5bc69d08d9b527c87f4d03fb2892f99dfde
MrDavisCompSci <38750430+MrDavisCompSci@users.noreply.github.com>
2026-05-05 10:31:15 +0000

    ,

--- Program.cs
+++ Program.cs
@@ -6,6 +6,34 @@ class Program
     {
         // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("Enter a sentence: ");
+        string userInput = Console.ReadLine();
+
+        Console.WriteLine(PigLatinSentence(userInput));
+    }
+
+    static string PigLatinWord(string s)
+    {
+        string result = "";
+
+        // WITCHCRAFT!
+        char firstLetter = s[0];
+        string restOfWord = s.Substring(1);
+
+        result = restOfWord + firstLetter + "ay";
+
+        return result;
+    }
+
+    static string PigLatinSentence(string sentence)
+    {
+        string result = "";
+        string[] sentenceArray = sentence.Split(' ');
+
+        foreach(string word in sentenceArray)
+        {
+            result = result + PigLatinWord(word) + " ";
+        }
+        return result;
     }
 }
